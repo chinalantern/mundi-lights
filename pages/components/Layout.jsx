7ce26c40ef289866ba5f2c12f2c406b9c8dbc534
Light <14278865+chinalantern@users.noreply.github.com>
2022-10-02 15:51:43 -0400

    implemented entire Layout wrapper Navbar & Footer

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Head from 'next/head' // In NEXTjs metadata about website before the body is injected this way
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Mundi Lights Store</title>
+      </Head>
+
+      <header>
+        <Navbar />
+      </header>
+
+      <main className="main-container">{children}</main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
