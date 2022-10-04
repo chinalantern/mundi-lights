209e66a2137cdd338c33a07a6d4b6dc9ecc152b1
Light <14278865+chinalantern@users.noreply.github.com>
2022-10-03 23:42:53 -0400

    implemented 1/2 cart logic Debug unexpected token

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Link from 'next/link'
+
+import { useStateContext } from '../context/StateContext'
+import { urlFor } from '../cms/SanityClient'
+
+import toast from 'react-hot-toast'
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from 'react-icons/ai'
+import { TiDeleteOutline } from 'react-icons/ti'
 
 const Cart = () => {
+  const cartRef = useRef()
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext()
+
   return (
-    <div>Cart</div>
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          className="cart-heading"
+          type="button"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your cart is empty</h3>
+
+            <Link href="/">
+              <button
+                className="btn"
+                type="button"
+                onClick={() => setShowCart(false)}
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                <img
+                  className="cart-product-image"
+                  src={urlFor(item?.image[0])}
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>${item.price}</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span className="minus" onClick="">
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num">0</span>
+                        <span className="plus" onClick="">
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+
+                    <button className="remove-item" type="button" onClick="">
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
